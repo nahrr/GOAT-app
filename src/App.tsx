71cfb998bb515b52f9c6439b5d827b708ccdd0be
Johan Kjellström <48734017+nahrr@users.noreply.github.com>
2021-09-05 00:35:21 +0200

    fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-
 import { Route, Switch } from "react-router";
 import "./App.css";
 import FrontPage from "./pages/FrontPage";
-import Nav from './components/NavBar';
+import Nav from "./components/NavBar";
 import TeamPage from "./pages/TeamPage";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-    <Nav/>
-    <Switch>
-      <Route path={"/rankings"}>
-        <FrontPage/>
-      </Route>
-      <Route path={"/team"}>
-        <TeamPage/>
-      </Route>
-    </Switch>
-  </div>
+      <Nav />
+      <Switch>
+        <Route path={"/rankings"}>
+          <FrontPage />
+        </Route>
+        <Route path={"/team"}>
+          <TeamPage />
+        </Route>
+        <Route path={"/"}>
+          <FrontPage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
